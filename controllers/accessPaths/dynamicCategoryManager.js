26aeb222035d05bd1285268b914e78a559b4c28f
Rakshitha-D <rakshithagowda.d@gmail.com>
2025-08-21 18:40:18 +0530

    SBCOSS-584: formatted dynamic category manager

--- controllers/accessPaths/dynamicCategoryManager.js
+++ controllers/accessPaths/dynamicCategoryManager.js
@@ -3,24 +3,23 @@ const { channelRead, frameworkRead } = require('../../helpers/learnerHelper');
 const debug = require('debug')('dynamicCategoryManager');
 
 class DynamicCategoryManager {
-    async getCategoriesForChannel(channelId) {
-        try {
-            const channelReadResponse = await channelRead({ channelId });
-            const frameworkName = _.get(channelReadResponse, 'data.result.channel.defaultFramework');
-            if (!frameworkName) {
-                throw new Error('Default framework missing');
-            }
+  async getCategoriesForChannel(channelId) {
+    try {
+      const channelReadResponse = await channelRead({ channelId });
+      const frameworkName = _.get(channelReadResponse, 'data.result.channel.defaultFramework');
+      if (!frameworkName) {
+        throw new Error('Default framework missing');
+      }
+      const frameworkReadResponse = await frameworkRead({ frameworkId: frameworkName });
+      const frameworkData = _.get(frameworkReadResponse, 'data.result.framework');
+      const categories = _.map(frameworkData.categories, 'code');
 
-            const frameworkReadResponse = await frameworkRead({ frameworkId: frameworkName });
-            const frameworkData = _.get(frameworkReadResponse, 'data.result.framework');
-            const categories = _.map(frameworkData.categories, 'code');
-            
-            return categories;
-        } catch (error) {
-            debug('Failed to fetch framework categories', error);
-            return [];
-        }
+      return categories;
+    } catch (error) {
+      debug('Failed to fetch framework categories', error);
+      return [];
     }
+  }
 
 }
 
